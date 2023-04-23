d37453056d7ede4f6245a068cd47a6b86632d37f
Jeff <jeffhernandez@nypl.org>
2023-04-23 17:13:08 -0400

    divide input into parts, assign variables in a map

--- interpreter.java
+++ interpreter.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
 public class Interpreter {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
-        System.out.println(inputString);
+        String[] inputLines = inputString.split(";");
+        Map<String, Integer> variablesMap = new HashMap<String, Integer>();
+        
     }
 
 
