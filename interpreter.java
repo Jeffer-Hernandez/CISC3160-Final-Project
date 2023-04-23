4f1126d37dd205d78c05d31890be9e6c9d4747ff
Jeff <jeffhernandez@nypl.org>
2023-04-23 16:58:40 -0400

    scanning input text, compiled and run for testing

--- interpreter.java
+++ interpreter.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class Interpreter {
 
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        System.out.println(inputString);
+    }
 
 
 
-    
 }
